98671f98cf3c7464363b1b2bf558415bd084c2a7
DuaaNawwas <duaa.nawwas@gmail.com>
2022-11-22 22:33:24 +0300

    fixed chart

--- fitness-front/src/components/profile/SavedCalories.jsx
+++ fitness-front/src/components/profile/SavedCalories.jsx
@@ -2,11 +2,17 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 export default function SavedCalories({ token }) {
+	const [inData, setInData] = useState();
+	const [outData, setOutData] = useState();
+
 	const [dates, setDates] = useState();
 	const [calories, setCalories] = useState();
 
 	const [dates2, setDates2] = useState();
 	const [caloriesOut, setCaloriesOut] = useState();
+
+	const [loading1, setLoading1] = useState(true);
+	const [loading2, setLoading2] = useState(true);
 
 	// Get user data from database
 	useEffect(() => {
@@ -26,14 +32,8 @@ export default function SavedCalories({ token }) {
 					return { ...i, created_at: i.created_at.split("T")[0] };
 				});
 
-				// Get an array of dates
-				const allDates = res.data.data.map(
-					(i) => i.attributes.created_at.split("T")[0]
-				);
-
-				// Remove duplicate dates
-				const uniqueDates = [...new Set(allDates)];
-				// setDates(uniqueDates);
+				// // Remove duplicate dates
+				// const uniqueDates = [...new Set(allDates)];
 
 				// Get calories sum for each date
 				const cals = Object.values(
@@ -44,12 +44,10 @@ export default function SavedCalories({ token }) {
 						return a;
 					}, {})
 				);
+				setInData(cals);
 				setDates(cals.map((i) => i.created_at));
 				setCalories(cals.map((i) => i.calories));
-				// console.log(allDates);
-				console.log(dataV);
-				console.log(cals);
-				console.log(uniqueDates);
+				setLoading1(false);
 			});
 	}, []);
 	// Get user data from database
@@ -79,17 +77,39 @@ export default function SavedCalories({ token }) {
 						return a;
 					}, {})
 				);
+				setOutData(cals);
+
 				setDates2(cals.map((i) => i.created_at));
 				setCaloriesOut(cals.map((i) => i.calories));
+				setLoading2(false);
 			});
 	}, []);
+
+	// Merge data from calories in and out based on date
+	const mergedData = inData?.map((i) => {
+		const date = outData?.find(({ created_at }) => created_at == i.created_at);
+		// console.log(date);
+		if (!date) {
+			return { ...i, caloriesOut: 0 };
+		} else {
+			return { ...i, caloriesOut: date.calories };
+		}
+	});
+	console.log("--------------");
+	console.log(inData);
+	console.log(outData);
+	console.log(mergedData);
 
 	// Data for Chart
 	const series = [
 		//data on the y-axis
 		{
 			name: "Calories consumed",
-			data: calories,
+			data: mergedData?.map((i) => i.calories),
+		},
+		{
+			name: "Calories burnt",
+			data: mergedData?.map((i) => i.caloriesOut),
 		},
 	];
 
@@ -133,72 +153,31 @@ export default function SavedCalories({ token }) {
 			categories: dates,
 		},
 	};
-	// Data for Chart
-	const series2 = [
-		//data on the y-axis
-		{
-			name: "Calories burnt",
-			data: caloriesOut,
-		},
-	];
 
-	const options2 = {
-		//data on the x-axis
-		chart: {
-			height: 350,
-			background: "#f3f3f3",
-			type: "line",
-			dropShadow: {
-				enabled: true,
-				color: "#000",
-				top: 18,
-				left: 7,
-				blur: 10,
-				opacity: 0.2,
-			},
-			toolbar: {
-				show: false,
-			},
-		},
-		colors: ["#37403d", "#545454"],
-		dataLabels: {
-			enabled: true,
-		},
-		stroke: {
-			curve: "smooth",
-		},
-
-		grid: {
-			borderColor: "#e7e7e7",
-			row: {
-				colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-				opacity: 0.5,
-			},
-		},
-		markers: {
-			size: 1,
-		},
-		xaxis: {
-			categories: dates2,
-		},
-	};
 	return (
 		<>
-			{calories?.length > 0 || caloriesOut?.length > 0 ? (
-				<div className="flex flex-wrap gap-10">
-					<div className="flex flex-col">
-						<p>Calories Consumed</p>
-						<Chart options={options} series={series} type="line" width="450" />
-					</div>
-					<div className="flex flex-col">
-						<p>Calories Burnt</p>
-						<Chart
-							options={options2}
-							series={series2}
-							type="line"
-							width="450"
+			{loading1 && loading2 ? (
+				<div role="status" className="flex justify-center py-5">
+					<svg
+						class="inline mr-2 w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-grape"
+						viewBox="0 0 100 101"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+							fill="currentColor"
 						/>
-					</div>
+						<path
+							d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+							fill="currentFill"
+						/>
+					</svg>
+					<span class="sr-only">Loading...</span>
+				</div>
+			) : calories?.length > 0 || caloriesOut?.length > 0 ? (
+				<div className="flex flex-col">
+					<Chart options={options} series={series} type="line" width="450" />
 				</div>
 			) : (
 				<h1 className="text-center">You didn't add any data</h1>
